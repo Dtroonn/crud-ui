66701d2f363a4b5915abefafca6db0da96998850
Dtroonn <Dtroonn@mail.ru>
2021-03-07 17:40:08 +0300

    Updates

---
+++ precache-manifest.4e0104232828d6afb8ed9d1c8eff5cd4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "173deeded60ceedfea4453b48080e968",
+    "url": "/crud-ui/index.html"
+  },
+  {
+    "revision": "0d35d11dbefce50267a6",
+    "url": "/crud-ui/static/js/2.8057b106.chunk.js"
+  },
+  {
+    "revision": "19f34a5ff8eb45ce8e78",
+    "url": "/crud-ui/static/js/main.4225f8c0.chunk.js"
+  },
+  {
+    "revision": "bd89f5c9793ffeaf7961",
+    "url": "/crud-ui/static/js/runtime-main.2b93e558.js"
+  }
+]);
